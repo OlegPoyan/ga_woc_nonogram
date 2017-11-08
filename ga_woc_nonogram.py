1a279cc7b7adb7974d482eea81db04b51fbb46e7
Oleg <oleg.poyan.dev@gmail.com>
2017-11-08 17:42:53 -0500

    Incorporated the fitness function into existing code

--- ga_woc_nonogram.py
+++ ga_woc_nonogram.py
@@ -27,18 +27,49 @@ class Nonogram(object):
         randomly."""
 
         return [[randint(0, 1) for x in range(0, grid_size)] for y in range(0, grid_size)]
+    @staticmethod
+    def calc_fitness(self):
+        """Returns the fitness for a particular grid"""
+
+        SQUARE_PENALTY = 1
+        GROUP_PENALTY = 2
+
+        score = 0
+
+        for index, row in enumerate(self.grid):
+            group_flag = False
+            filled_count = 0
+            group_count = 0
+            row_square_number = 0
+
+            for square in row:
+                # Calculate number of groups and number of FILLED squares present
+                # in the row
+                if square == FILLED:
+                    if not group_flag:
+                        group_flag = True
+                        group_count += 1
+                    filled_count += 1
+                else:
+                    if group_flag:
+                        group_flag = False
+            for number in self.row_numbers[index]:
+                row_square_number += number
+            print(str(filled_count) + " - " + str(row_square_number) + " " + str(group_count) + " - " + str(len(self.row_numbers[index])))
+            score += SQUARE_PENALTY * abs(filled_count - row_square_number) + GROUP_PENALTY * abs(group_count - len(self.row_numbers[index]))
+        return score
 
     def __init__(self, nonogram_size):
         """Return board with dimensions of size nonogram_size. row_numbers,
             column_number are hardcoded for now."""
         # create random id
         self.nonogram_id = uuid.uuid4()
-        self.row_numbers = [(2), (2), (2)]
-        self.column_numbers = [(1, 1), (3), (1)]
+        self.row_numbers = [(2,), (2,), (2,)]
+        self.column_numbers = [(1, 1), (3,), (1,)]
         self.nonogram_size = nonogram_size
         self.grid = Nonogram.create_rand_grid(nonogram_size)
-        #TODO
-        self.fitness = 999
+        self.fitness = Nonogram.calc_fitness(self)
+
     def draw_nonogram(self):
         """ Create an PNG format image of grid"""
         image = Image.new("RGB", (self.nonogram_size * 50, self.nonogram_size * 50), (255, 255, 255))
@@ -56,12 +87,16 @@ class Nonogram(object):
                 draw.rectangle(coord, fill=(0, 0, 0))
         return image
 
+
 def create_population(board_size, population_size):
     """Returns a list of randomly filled Nonogram puzzle objects"""
     return [Nonogram(board_size) for x in range(0, population_size)]
 
-# def ga_algorithm():
+def ga_algorithm(board_size, population_size):
+    """ga algorithm to find a solution for Nonogram puzzle"""
+    for index, board in enumerate(create_population(board_size, population_size)):
+        image = board.draw_nonogram()
+        image.save("%s_%d.png" % ("nono", index))
+        print("Board #" + str(index )+ " " + str(board.fitness))
 
-for board in create_population(10, 10):
-    image = board.draw_nonogram()
-    image.save("%s_%d.png" % ("nono", board.nonogram_id))
+ga_algorithm(3, 10)
