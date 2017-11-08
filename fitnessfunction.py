47658cdb7f39ebb017fa5a03b65a7d7338610a7c
rewted <rewtguy@gmail.com>
2017-11-08 14:27:27 -0500

    Add files via upload

--- fitnessfunction.py
+++ fitnessfunction.py
@@ -6,20 +6,22 @@ def fitness(board):
     groupFlag = False
     score = 0
     
+    # checks row by row to get a score based on number of groups
+    # and number of on squares needed compared to how many found
     for i in range (0, board.nonogram_size):
         squareCount = 0
         groupCount = 0
         squares = 0
         groups = 0
         for y in board.row_numbers[i]:
-            groupCount++
+            groupCount+=1
             squareCount = squareCount + y
             
         for j in range (0, board.nonogram_size):
             if (board.grid[i][j] == 1):
-                squares++
-                if (!groupFlag):
-                    groups++
+                squares+=1
+                if not groupFlag:
+                    groups+=1
                 groupFlag = True
             if (board.grid[i][j] == -1):
                 if (groupFlag):
@@ -28,20 +30,22 @@ def fitness(board):
         groups = abs(groups  - groupCount)
         score = score + (squarePenalty * squares + groupPenalty * groups)
 
+    # checks column by column to get a score based on number of groups
+    # and number of on squares needed compared to how many found
     for i in range (0, board.nonogram_size):
         squareCount = 0
         groupCount = 0
         squares = 0
         groups = 0
         for y in board.column_numbers[i]:
-            groupCount++
+            groupCount+=1
             squareCount = squareCount + y
 
         for j in range (0, board.nonogram_size):
             if (board.grid[j][i] == 1):
-                square++
-                if (!groupFlag):
-                    groups++
+                square+=1
+                if not groupFlag:
+                    groups+=1
                 groupFlag = True
             if (board.grid[j][i] == -1):
                 if (groupFlag):
@@ -51,5 +55,5 @@ def fitness(board):
         groups = abs(groups - groupCount)
         score = score + (squarePenalty * squares + groupPenalty * groups)
         
-    
+    # updates the board fitness with the b
     board.fitness = score
